a3c616743643bce7588517ca2112e26ea022ae73
kailash024 <kailash@ramailo.tech>
2024-07-02 12:21:25 +0000

    chat services updated using langchain

--- mle_core/chat/chat_service.py
+++ mle_core/chat/chat_service.py
@@ -1,6 +1,7 @@
 from mle_core.config import get_llm_connector
-from mle_core.utils import format_prompt, handle_llm_response, setup_logging
+from mle_core.utils import setup_logging
 from langchain_core.prompts import ChatPromptTemplate
+from typing import List
 
 logger = setup_logging()
 
@@ -8,68 +9,93 @@ class ChatService:
     def __init__(self, llm_type):
         self.llm_connector = get_llm_connector(llm_type)
 
-    
-    def get_lecl_chain(self):
+    def get_lecl_chain(self, model_name, is_structured=False, pydantic_model=None,**kwargs):
+        if is_structured and pydantic_model is None:
+            raise ValueError("pydantic_model cannot be None when is_structured is True")
+        
         prompt = ChatPromptTemplate.from_messages(
             [
-                ("system","{system_message}"),
-                ("human","{user_message}")
+                ("system", "{system_message}"),
+                ("human", "{user_message}")
             ]
         )
-        llm = self.llm_connector.get_connection()
-        chain = prompt | llm #structured output
+        llm = self.llm_connector.get_connection(model_name=model_name,**kwargs)
+        
+        if is_structured and pydantic_model is not None:
+            llm = llm.with_structured_output(pydantic_model)
+        
+        
+        chain = prompt | llm
         return chain
     
-    async def async_invoke(self,chain,input):
+    async def async_invoke(self, chain, input):
         return await chain.ainvoke(input)
 
     async def async_stream(self, chain, input):
-        stream_output = await chain.astream(input)
-        output_content = []
-        async for s in stream_output:
-            output_content.append(s.content)
-        return output_content
+        async for s in chain.astream(input):
+            yield s     
 
+    async def async_batch(self, chain, inputs: List[dict]):
+        return await chain.abatch(inputs)
 
-    async def async_batch(self,chain,input):
-        return await chain.abatch(input)
-    
-    def sync_batch(self,chain,input):
-        return chain.batch(input)
-    
-    def sync_invoke(self,chain,input):
+    def sync_batch(self, chain, inputs: List[dict]):
+        return chain.batch(inputs)
+
+    def sync_invoke(self, chain, input):
         return chain.invoke(input)
-    
-    def sync_stream(self,chain,input):
-        output_content = []
-        for s in chain.stream(input):
-            output_content.append(s.content)
-        return output_content
-    
-    async def get_response(self,method, response_method, input):
-        chain = self.get_lecl_chain()
 
-        if method == "sync":
-            if response_method == "invoke":
-                response =  self.sync_invoke(chain, input)
-                return response.content
-            elif response_method == "batch":
-                response = self.sync_batch(chain, input)
-                return response.content
-            elif response_method == "stream":
-                return self.sync_stream(chain, input)
-            else:
-                raise ValueError("Invalid response_method for sync")
-        elif method == "async":
-            if response_method == "invoke":
-                response = await self.async_invoke(chain, input)
-                return response.content
-            elif response_method == "stream":
-                return await self.async_stream(chain, input)
-            elif response_method == "batch":
-                response = await self.async_batch(chain, input)
-                return response.content
-            else:
-                raise ValueError("Invalid response_method for async")
-        
-    
+    def sync_stream(self, chain, input):
+        for s in chain.stream(input):
+            yield s   
+
+    def get_sync_response(self, method, response_method, input, model_name, is_structured=False, pydantic_model=None, **kwargs):
+        chain = self.get_lecl_chain(model_name=model_name, is_structured=is_structured, pydantic_model=pydantic_model, **kwargs)
+        if response_method == "invoke":
+            response = self.sync_invoke(chain, input)
+            if not is_structured:
+                response = response.content
+            return response
+        elif response_method == "batch":
+            response = self.sync_batch(chain, input)
+            if not is_structured:
+                response = [msg.content for msg in response]
+            return response
+        elif response_method == "stream":
+            output_content = []
+            for s in self.sync_stream(chain, input):
+                if not is_structured:
+                    output_content.append(s.content)
+                else:
+                    output_content=s
+            if not is_structured:
+                return "".join(output_content)
+            return output_content
+        else:
+            raise ValueError("Invalid response_method for sync")
+
+
+    async def get_async_response(self, method, response_method, input, model_name, is_structured=False, pydantic_model=None,  **kwargs):
+        chain = self.get_lecl_chain(model_name=model_name, is_structured=is_structured, pydantic_model=pydantic_model,**kwargs)
+        if response_method == "invoke":
+            response = await self.async_invoke(chain, input)
+            if not is_structured:
+                response = response.content
+            return response
+        elif response_method == "batch":
+            response = await self.async_batch(chain, input)
+            if not is_structured:
+                response = [msg.content for msg in response]
+            return response
+
+        elif response_method == "stream":
+            output_content = []
+            async for s in self.async_stream(chain, input):
+                if not is_structured:
+                    output_content.append(s.content)
+                else:
+                    output_content=s      
+            if not is_structured:
+                return "".join(output_content)    
+            return output_content
+        else:
+            raise ValueError("Invalid response_method for async")
